c81aa63fbcc3c26a1c41a48294434156291cb11c
Joel Eager <jee7291@rit.edu>
2018-04-04 14:53:27 -0400

    Cleaned up and fixed code

---
+++ src/CollisionDetectorTest.java
@@ -0,0 +1,38 @@
+class CollisionDetectorTest {
+    public static void main(String[] args) {
+        int iterations;
+
+        try {
+            iterations = Integer.parseInt(args[0]);
+        } catch (Exception e) {
+            System.out.println("Usage:\njava CollisionDetectorTest [iterations]");
+            return;
+        }
+
+        System.out.println("Benchmarking sat.hasCollided() using a shell and tank...");
+        System.out.println("Using $iterations iterations\n");
+
+        System.out.println("Time with maxDist: " + runTest(iterations, 15.56));
+        System.out.println("Time without:      " + runTest(iterations, null));
+    }
+
+    private static double runTest(int iterations, Double maxDist) {
+        long startTime = System.currentTimeMillis();
+
+        for (int i = 0; i <iterations; i++) {
+            Sat.Vector[] tank = { new Sat.Vector(195.0, 95.0), new Sat.Vector(205.0, 95.0),
+                    new Sat.Vector(205.0, 105.0), new Sat.Vector(195.0, 105.0) };
+            Sat.Vector[] shell = { new Sat.Vector(99.5, 99.5), new Sat.Vector(100.5, 99.5),
+                    new Sat.Vector(100.5, 100.5), new Sat.Vector(99.5, 100.5) };
+
+            while (Sat.hasCollided(tank, shell, maxDist)) {
+                // Move the shell
+                for (Sat.Vector vector : shell) {
+                    vector.x += 1.0;
+                }
+            }
+        }
+
+        return (System.currentTimeMillis() - startTime) / 1000.0;
+    }
+}
--- src/sat.java
+++ src/sat.java
@@ -1,14 +1,25 @@
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
-class Vector {
-    double x, y;
+class Sat {
+    static class Vector {
+        double x, y;
 
-    Vector(double x, double y) {
-        this.x = x;
-        this.y = y;
+        Vector(double x, double y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        /**
+         * Returns a new vector which is orthogonal to the current vector
+         */
+        Vector orthogonal() {
+            return new Vector(y, -x);
+        }
     }
 
-    Boolean hasCollided(Vector[] poly1, Vector[] poly2, Double maxDist) {
+    public static Boolean hasCollided(Vector[] poly1, Vector[] poly2, Double maxDist) {
         // Do an optimization check using the maxDist
         if (maxDist != null) {
             if (Math.pow(poly1[1].x - poly2[0].x, 2) + Math.pow(poly1[1].y - poly2[0].y, 2) <= Math.pow(maxDist, 2)) {
@@ -23,13 +34,13 @@ class Vector {
         }
     }
 
-    private Boolean runSAT(Vector[] poly1, Vector[] poly2) {
+    private static Boolean runSAT(Vector[] poly1, Vector[] poly2) {
         // Implements the actual SAT algorithm
         ArrayList<Vector> edges = polyToEdges(poly1);
         edges.addAll(polyToEdges(poly2));
         Vector[] axes = new Vector[edges.size()];
         for (int i = 0; i < edges.size(); i++) {
-            axes[i] = orthogonal(edges.get(i));
+            axes[i] = edges.get(i).orthogonal();
         }
 
         for (Vector axis : axes) {
@@ -46,14 +57,14 @@ class Vector {
     /**
      * Returns a vector going from point1 to point2
      */
-    private Vector edgeVector(Vector point1, Vector point2) {
+    private static Vector edgeVector(Vector point1, Vector point2) {
         return new Vector(point2.x - point1.x, point2.y - point1.y);
     }
 
     /**
      * Returns an array of the edges of the poly as vectors
      */
-    private ArrayList<Vector> polyToEdges(Vector[] poly) {
+    private static ArrayList<Vector> polyToEdges(Vector[] poly) {
         ArrayList<Vector> vectors = new ArrayList<>(poly.length);
         for (int i = 0; i < poly.length; i++) {
             vectors.add(edgeVector(poly[i], poly[(i + 1) % poly.length]));
@@ -62,34 +73,27 @@ class Vector {
     }
 
     /**
-     * Returns a new vector which is orthogonal to the given vector
-     */
-    private Vector orthogonal(Vector vector) {
-        return new Vector(vector.y, -vector.x);
-    }
-
-    /**
      * Returns the dot (or scalar) product of the two vectors
      */
-    private double dotProduct(Vector vector1, Vector vector2) {
+    private static double dotProduct(Vector vector1, Vector vector2) {
         return vector1.x * vector2.x + vector1.y * vector2.y;
     }
 
     /**
      * Returns a vector showing how much of the poly lies along the axis
      */
-    private Vector project(Vector[] poly, Vector axis) {
-        double[] dots = new double[poly.length];
-        for (int i = 0; i < poly.length; i++) {
-            dots[i] = dotProduct(poly[i], axis);
+    private static Vector project(Vector[] poly, Vector axis) {
+        List<Double> dots = new ArrayList<>();
+        for (Vector vector : poly) {
+            dots.add(dotProduct(vector, axis));
         }
-        return Vector(dots.min(), dots.max());
+        return new Vector(Collections.min(dots), Collections.max(dots));
     }
 
     /**
      * Returns a boolean indicating if the two projections overlap
      */
-    private boolean overlap(Vector projection1, Vector projection2) {
+    private static boolean overlap(Vector projection1, Vector projection2) {
         return projection1.x <= projection2.y &&
                 projection2.x <= projection1.y;
     }
